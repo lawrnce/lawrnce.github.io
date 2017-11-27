36a4e34113431002d0316806e6d1103721e041ac
Lawrence Tran <lawrence.c.tran@outlook.com>
2017-11-27 13:16:12 -0600

    Changed CNAME

--- gatsby-config.js
+++ gatsby-config.js
@@ -13,6 +13,7 @@ module.exports = {
     `gatsby-plugin-catch-links`,
     `gatsby-plugin-styled-components`,
     `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-favicon`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
