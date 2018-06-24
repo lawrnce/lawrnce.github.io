65455a9d97b48205d78866e72259afb0f4505ef1
Lawrence Tran <git@lawrnce.com>
2018-06-24 18:20:49 -0500

    Add windmill gifs

--- src/components/hero.js
+++ src/components/hero.js
@@ -2,16 +2,22 @@ import React from 'react'
 import styled from 'styled-components'
 
 const Wrapper = styled.div`
-  display: grid;
   grid-template-columns: 1fr;
   grid-template-areas:
-    'icon';
+    'banner'
+    'description';
 `;
 
-const Banner = styled.img`
-  grid-area: icon;
+const Banner = styled.div`
+  grid-area: banner;
   width: 100%;
   height: auto;
+  line-height: 0;
+  grid-template-columns: 1fr 1fr;
+  grid-template-areas:
+    '1 2'
+    'name name'
+    '3 4';
 
   @media (min-width: 480px) {
     width: 420px;
@@ -20,7 +26,39 @@ const Banner = styled.img`
   }
 `;
 
+const Windmill1 = styled.img`
+  grid-area: 1;
+  width: 50%;
+  height: auto;
+  margin: 0 0 0 0;
+`;
+
+const Windmill2 = styled.img`
+  grid-area: 2;
+  width: 50%;
+  margin: 0 0 0 0;
+`;
+
+const Windmill3 = styled.img`
+  grid-area: 3;
+  width: 50%;
+  margin: 0 0 0 0;
+`;
+
+const Windmill4 = styled.img`
+  grid-area: 4;
+  width: 50%;
+  margin: 0 0 0 0;
+`;
+
+const Name = styled.img`
+  grid-area: name;
+  width: 100%;
+  margin: 0 0 0 0;
+`;
+
 const Description = styled.h1`
+  grid-area: 4;
   text-align: center;
   margin-right: 16px;
   margin-left: 16px;
@@ -28,7 +66,13 @@ const Description = styled.h1`
 
 const Hero = ({ props }) =>
   <Wrapper>
-    <Banner src={ require('../assets/gif/lawrencetran.gif') } />
+    <Banner>
+      <Windmill1 src={ require('../assets/gif/windmill-1.gif') } />
+      <Windmill2 src={ require('../assets/gif/windmill-2.gif') } />
+      <Name src={ require('../assets/gif/name.gif') } />
+      <Windmill3 src={ require('../assets/gif/windmill-3.gif') } />
+      <Windmill4 src={ require('../assets/gif/windmill-4.gif') } />
+    </Banner>
     <Description>
       I make music, apps, and games.
     </Description>
